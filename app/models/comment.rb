2aa3fe404cd018be43fcfa231323427ecfde439b
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-23 05:09:06 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  def comment_user_attributes=(comment_user_attribute)
+    user = User.find_or_create_by(username: comment_user_attribute[:username])
+    comment.user= user
+  end
 end
