db17d0296fe8477db35040d6837fd6b119ea0734
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-23 05:12:37 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,7 @@ class Comment < ActiveRecord::Base
   accepts_nested_attributes_for :user
 
   def comment_user_attributes=(comment_user_attribute)
+    binding.pry
     if !comment_user_attribute[:username].blank?
       user = User.find_or_create_by(username: comment_user_attribute[:username])
       comment.user= user
