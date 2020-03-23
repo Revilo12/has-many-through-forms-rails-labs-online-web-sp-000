fd8162075bd7192d33a47a16d1de4a22cf3dbe85
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-23 02:32:41 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :comments, :reject_if => :reject_comment
-
+  accepts_nested_attributes_for :categories, :reject_if => :reject_category
   def reject_comment(attributed)
     attributed['content'].blank?
+  end
+
+  def reject_category(attributed)
+    attributed['name'].blank?
   end
 
   def category_name=(name)
