60ea84a128577fa29e45110d83c0d05ea4dbc230
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-23 03:15:16 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :comments, :reject_if => :reject_comment
-  accepts_nested_attributes_for :categories, :reject_if => :reject_category
-  def reject_comment(attributed)
+  accepts_nested_attributes_for :comments, :categories
     attributed['content'].blank?
   end
 
